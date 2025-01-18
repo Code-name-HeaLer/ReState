eff081b88b648025aceed3fc00d5af526285f899
CodeNameHealer <swagatprasad3344@gmail.com>
2025-01-18 15:29:20 +0530

    Home screen scroll behavior done

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
@@ -6,17 +6,30 @@ import icons from "@/constants/icons";
 import Search from "@/components/Search";
 import { Card, FeaturedCard } from "@/components/Cards";
 import Filters from "@/components/Filters";
+import { useGlobalContext } from "@/lib/global-provider";
 
 export default function Index() {
+
+  const {user} = useGlobalContext();
+
   return (
     <SafeAreaView className="bg-white h-full">
-      <View className="px-5">
+      <FlatList 
+      data = {[1,2,3,4]} 
+      renderItem={({item}) => <Card/>}
+      keyExtractor={(item) => item.toString()}
+      numColumns={2}
+      contentContainerClassName="pb-32"
+      columnWrapperClassName="flex gap-5 px-5"
+      showsVerticalScrollIndicator={false}
+      ListHeaderComponent={
+        <View className="px-5">
         <View className="flex flex-row items-center justify-between mt-1.5">
-          <View className="flex flex-row">
-            <Image source={images.avatar} className="size-12 rounded-full" />
+          <View className="flex flex-row items-center">
+            <Image source={{uri: user?.avatar}} className="size-12 rounded-full" />
             <View className="flex flex-col items-start ml-2 justify-center">
               <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
-              <Text className="text-base font-rubik-medium text-black-300">Adrian</Text>
+              <Text className="text-base font-rubik-medium text-black-300">{user?.name}</Text>
             </View>
           </View>
           <Image source={icons.bell} className="size-6" />
@@ -29,10 +42,17 @@ export default function Index() {
               <Text className="text-base font-rubik-bold text-primary-300 mb-1">See All</Text>
             </TouchableOpacity>
           </View>
-          <View className="flex flex-row gap-5 mt-2.5">
-          <FeaturedCard />
-          <FeaturedCard />
-          </View>
+
+          <FlatList 
+          data={[1,2,3,4]} 
+          renderItem={({item}) => <FeaturedCard/>}
+          keyExtractor={(item) => item.toString()}
+          horizontal
+          bounces={false}
+          showsHorizontalScrollIndicator={false}
+          contentContainerClassName="flex gap-5 mt-5"
+          />
+
         </View>
 
         <View className="flex flex-row items-center justify-between">
@@ -44,12 +64,13 @@ export default function Index() {
 
           <Filters/>
 
-          <View className="flex flex-row gap-5">
-            <Card/>
-            <Card/>
-          </View>
+
 
       </View>
+      }
+
+      />
+      
 
     </SafeAreaView>
   );
