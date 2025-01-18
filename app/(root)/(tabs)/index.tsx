763d03105c625b51b00c8e03f4a5d240cfac5f29
CodeNameHealer <swagatprasad3344@gmail.com>
2025-01-18 20:06:49 +0530

    Imp: DataBase Seeding completed

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { Button, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
@@ -7,6 +7,7 @@ import Search from "@/components/Search";
 import { Card, FeaturedCard } from "@/components/Cards";
 import Filters from "@/components/Filters";
 import { useGlobalContext } from "@/lib/global-provider";
+import seed from "@/lib/seed";
 
 export default function Index() {
 
@@ -44,7 +45,7 @@ export default function Index() {
           </View>
 
           <FlatList 
-          data={[1,2,3,4]} 
+          data={[5,6,7]} 
           renderItem={({item}) => <FeaturedCard/>}
           keyExtractor={(item) => item.toString()}
           horizontal
